61ed4b3b43c881ce4e5fd4e4cfac67617a2aed17
Emily Muller <emilymuller1991@gmail.com>
2024-02-02 16:54:47 +0000

    minor changes to allow more flexibility when running other portfolios

--- idr/__main__.py
+++ idr/__main__.py
@@ -68,41 +68,41 @@ if __name__ == "__main__":
     # grant_ids, pub_ids = get_sanger_ids(fname=args.input_path)
 
     # calculate knowledge_integration/reference list to allow for cosine similarity calculation
-    # df = calculate_diversity(
-    #     pub_ids,
-    #     dim="knowledge_integration",
-    #     S3_OUTPUT_FOLDER=args.S3_OUTPUT_FOLDER,
-    #     parallel=args.parallel,
-    #     weighted=None,
-    # )
-    # cosine_matrix(df, args.S3_OUTPUT_FOLDER)
-    # citation_matrix(df, args.S3_OUTPUT_FOLDER)
+    df = calculate_diversity(
+        pub_ids,
+        dim="knowledge_integration",
+        S3_OUTPUT_FOLDER=args.S3_OUTPUT_FOLDER,
+        parallel=args.parallel,
+        weighted=None,
+    )
+    cosine_matrix(df, args.S3_OUTPUT_FOLDER)
+    citation_matrix(df, args.S3_OUTPUT_FOLDER)
 
-    # # calculate diversity using similarity weights for portfolio
-    # dimensions = ["grantees_fields", "knowledge_integration", "knowledge_diffusion"]
-    # for dim in dimensions:
-    #     calculate_diversity(
-    #         pub_ids,
-    #         grant_ids,
-    #         dim=dim,
-    #         S3_OUTPUT_FOLDER=args.S3_OUTPUT_FOLDER,
-    #         parallel=args.parallel,
-    #         weighted=True,
-    #     )
+    # calculate diversity using similarity weights for portfolio
+    dimensions = ["grantees_fields", "knowledge_integration", "knowledge_diffusion"]
+    for dim in dimensions:
+        calculate_diversity(
+            pub_ids,
+            grant_ids,
+            dim=dim,
+            S3_OUTPUT_FOLDER=args.S3_OUTPUT_FOLDER,
+            parallel=args.parallel,
+            weighted=True,
+        )
 
     # output data summaries as html report
     summary_reports(args.S3_OUTPUT_FOLDER, basic=True, diversity=True)
 
     # aggregate and group to grant-level
-    # aggregate_grant_level(args.S3_OUTPUT_FOLDER)
-    # summary_report_grant_level(args.S3_OUTPUT_FOLDER, args.scheme_mapping)
+    aggregate_grant_level(args.S3_OUTPUT_FOLDER)
+    summary_report_grant_level(args.S3_OUTPUT_FOLDER, args.scheme_mapping)
 
     # output plotly visualisations as html
-    # print("Computing final plotly visualisations")
-    # idr_types(args.S3_OUTPUT_FOLDER, args.scheme_mapping, args.award_mapping)
-    # topic_treemap(args.S3_OUTPUT_FOLDER, args.award_mapping)
-    # topic_diversity(args.S3_OUTPUT_FOLDER, args.award_mapping)
-    # subfield_diversity(args.S3_OUTPUT_FOLDER)
-    # subfield_treemap(args.S3_OUTPUT_FOLDER, args.award_mapping)
+    print("Computing final plotly visualisations")
+    idr_types(args.S3_OUTPUT_FOLDER, args.scheme_mapping, args.award_mapping)
+    topic_treemap(args.S3_OUTPUT_FOLDER, args.award_mapping)
+    topic_diversity(args.S3_OUTPUT_FOLDER, args.award_mapping)
+    subfield_diversity(args.S3_OUTPUT_FOLDER)
+    subfield_treemap(args.S3_OUTPUT_FOLDER, args.award_mapping)
 
     print("Pipeline Complete in %s minutes" % str((time.time() - start_time) / 60))
